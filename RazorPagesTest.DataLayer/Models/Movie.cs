1bc89e7a4bd36843529c0d9074fde4549aadebf7
Vladislav Kurkotov <kypa_2@ukr.net>
2017-12-07 00:02:50 +0200

    Add EF core

--- RazorPagesTest.DataLayer/Models/Movie.cs
+++ RazorPagesTest.DataLayer/Models/Movie.cs
@@ -6,5 +6,14 @@ namespace RazorPagesTest.DataLayer.Models
 {
     public class Movie
     {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public string Genre { get; set; }
+
+        public decimal Price { get; set; }
     }
 }
--- RazorPagesTest.Web/Startup.cs
+++ RazorPagesTest.Web/Startup.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using RazorPagesTest.DataLayer.Models;
 
 namespace RazorPagesTest.Web
 {
@@ -22,6 +24,8 @@ namespace RazorPagesTest.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddDbContext<MovieContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("MovieContext"), p => p.MigrationsAssembly("RazorPagesTest.DataLayer")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
