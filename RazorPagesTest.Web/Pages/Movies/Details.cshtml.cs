671aa48dac33e112b9ddf43a3f43089210c45381
Vladislav Kurkotov <kypa_2@ukr.net>
2017-12-07 00:37:36 +0200

    Add scaffolded pages

---
+++ RazorPagesTest.Web/Pages/Movies/Details.cshtml.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesTest.DataLayer.Models;
+
+namespace RazorPagesTest.Web.Pages.Movies
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly RazorPagesTest.DataLayer.Models.MovieContext _context;
+
+        public DetailsModel(RazorPagesTest.DataLayer.Models.MovieContext context)
+        {
+            _context = context;
+        }
+
+        public Movie Movie { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Movie = await _context.Movies.SingleOrDefaultAsync(m => m.Id == id);
+
+            if (Movie == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
---
+++ RazorPagesTest.Web/Pages/Movies/Edit.cshtml.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesTest.DataLayer.Models;
+
+namespace RazorPagesTest.Web.Pages.Movies
+{
+    public class EditModel : PageModel
+    {
+        private readonly RazorPagesTest.DataLayer.Models.MovieContext _context;
+
+        public EditModel(RazorPagesTest.DataLayer.Models.MovieContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Movie Movie { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Movie = await _context.Movies.SingleOrDefaultAsync(m => m.Id == id);
+
+            if (Movie == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Movie).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MovieExists(Movie.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool MovieExists(int id)
+        {
+            return _context.Movies.Any(e => e.Id == id);
+        }
+    }
+}
