288a5ba0ac2541fed8e49395f8f174821b5a1d69
Vladislav Kurkotov <kypa_2@ukr.net>
2017-12-17 23:21:22 +0200

    Add data annotations

--- RazorPagesTest.Web/Models/MovieModel.cs
+++ RazorPagesTest.Web/Models/MovieModel.cs
@@ -8,16 +8,24 @@ namespace RazorPagesTest.Web.Models
     {
         public int Id { get; set; }
 
+        [StringLength(60, MinimumLength = 1)]
+        [Required]
         public string Title { get; set; }
 
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
 
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        [Required]
+        [StringLength(30)]
         public string Genre { get; set; }
 
+        [DataType(DataType.Currency)]
         public decimal Price { get; set; }
 
+        [StringLength(5)]
+        [Required]
         public string Rating { get; set; }
 
         public static MovieModel FromMovie(Movie movie)
