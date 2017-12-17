262388618483cb1477ba3426693c8c1f6b5b6342
Vladislav Kurkotov <kypa_2@ukr.net>
2017-12-18 00:17:59 +0200

    Add Schedule

---
+++ RazorPagesTest.Web/Models/ScheduleModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using RazorPagesTest.DataLayer.Models;
+
+namespace RazorPagesTest.Web.Models
+{
+    public class ScheduleModel
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string PublicSchedule { get; set; }
+
+        [Display(Name = "Public Schedule Size (bytes)")]
+        [DisplayFormat(DataFormatString = "{0:N1}")]
+        public long PublicScheduleSize { get; set; }
+
+        public string PrivateSchedule { get; set; }
+
+        [Display(Name = "Private Schedule Size (bytes)")]
+        [DisplayFormat(DataFormatString = "{0:N1}")]
+        public long PrivateScheduleSize { get; set; }
+
+        [Display(Name = "Uploaded (UTC)")]
+        [DisplayFormat(DataFormatString = "{0:F}")]
+        public DateTime UploadDt { get; set; }
+
+        public static ScheduleModel FromSchedule(Schedule schedule)
+        {
+            return new ScheduleModel
+            {
+                Id = schedule.Id,
+                Title = schedule.Title,
+                PublicSchedule = schedule.PublicSchedule,
+                PublicScheduleSize = schedule.PublicScheduleSize,
+                PrivateSchedule = schedule.PrivateSchedule,
+                PrivateScheduleSize = schedule.PrivateScheduleSize,
+                UploadDt = schedule.UploadDt
+            };
+        }
+
+        public Schedule ToSchedule()
+        {
+            return new Schedule
+            {
+                Id = Id,
+                Title = Title,
+                PublicSchedule =  PublicSchedule,
+                PublicScheduleSize = PublicScheduleSize,
+                PrivateSchedule = PrivateSchedule,
+                PrivateScheduleSize = PrivateScheduleSize,
+                UploadDt = UploadDt
+            };
+        }
+    }
+}
---
+++ RazorPagesTest.Web/Pages/Schedules/Index.cshtml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesTest.Web.Models;
+using RazorPagesTest.Web.Utilities;
+
+namespace RazorPagesTest.Web.Pages.Schedules
+{
+    public class IndexModel : PageModel
+    {
+        private readonly DataLayer.Models.MovieContext _context;
+
+        public IndexModel(DataLayer.Models.MovieContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public FileUpload FileUpload { get; set; }
+
+        public IList<ScheduleModel> Schedule { get; private set; }
+
+        public async Task OnGetAsync()
+        {
+            Schedule = await _context
+                .Schedules
+                .AsNoTracking()
+                .Select(x => ScheduleModel.FromSchedule(x))
+                .ToListAsync();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            // Perform an initial check to catch FileUpload class
+            // attribute violations.
+            if (!ModelState.IsValid)
+            {
+                Schedule = await _context
+                    .Schedules
+                    .AsNoTracking()
+                    .Select(x => ScheduleModel.FromSchedule(x))
+                    .ToListAsync();
+                return Page();
+            }
+
+            var publicScheduleData =
+                await FileHelpers.ProcessFormFile(FileUpload.UploadPublicSchedule, ModelState);
+
+            var privateScheduleData =
+                await FileHelpers.ProcessFormFile(FileUpload.UploadPrivateSchedule, ModelState);
+
+            // Perform a second check to catch ProcessFormFile method
+            // violations.
+            if (!ModelState.IsValid)
+            {
+                Schedule = await _context
+                    .Schedules
+                    .AsNoTracking()
+                    .Select(x => ScheduleModel.FromSchedule(x))
+                    .ToListAsync();
+                return Page();
+            }
+
+            var scheduleModel = new ScheduleModel()
+            {
+                PublicSchedule = publicScheduleData,
+                PublicScheduleSize = FileUpload.UploadPublicSchedule.Length,
+                PrivateSchedule = privateScheduleData,
+                PrivateScheduleSize = FileUpload.UploadPrivateSchedule.Length,
+                Title = FileUpload.Title,
+                UploadDt = DateTime.UtcNow
+            };
+
+            _context.Schedules.Add(scheduleModel.ToSchedule());
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
